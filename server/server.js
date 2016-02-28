85b648a38b73d952d0c96c1ec057ff56bef527e3
Christopher Su <christophersu9@gmail.com>
2016-02-27 20:58:45 -0800

    does stuff

---
+++ server/server.js
@@ -0,0 +1,24 @@
+"use strict";
+const port = process.env.PORT || 3000;
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false } ));
+app.use(express.static('static'));
+
+const server = app.listen(port, () => {
+  console.log('Listening on *:' + port);
+});
+
+const io = require('socket.io')(server);
+io.on('connection', function(socket){
+  socket.on('data message', function(msg){
+    socket.broadcast.emit('data message', msg);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/views/index.html');
+});
