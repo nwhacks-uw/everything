2430b259aeed6e706818822b7b2bc87ed91a06e1
nmusgrave <naomi.g.musgrave@gmail.com>
2016-02-28 10:06:12 -0800

    fix printing

--- private/js/main.js
+++ private/js/main.js
@@ -4,8 +4,7 @@ var crypto = require('./crypto');
 
 // open file indicated by user, and encrypt
 window.ReadFile = function(data){
-  // console.log('Hello OpenFile, got ' + data);
+  console.log('encoding');
   var safe = crypto.encrypt(data);
-  // console.log('decoded ' + crypto.decrypt(safe));
 };
 
