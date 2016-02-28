e0ebeae9d642c768274c83dd9f761f00993a0ecc
nmusgrave <naomi.g.musgrave@gmail.com>
2016-02-28 10:10:53 -0800

    remove crypto

--- private/js/main.js
+++ private/js/main.js
@@ -1,10 +1,8 @@
 require('./shim');
-require('./video');
-var crypto = require('./crypto');
+var video = require('./video');
 
 // open file indicated by user, and encrypt
 window.ReadFile = function(data){
   console.log('encoding');
-  var safe = crypto.encrypt(data);
+  // TODO- pass into video
 };
-
